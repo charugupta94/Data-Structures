cd668a0aae013fa2f02368bbe48b8072a417bd60
Charu Gupta <118685509+charugupta94@users.noreply.github.com>
2025-06-16 21:53:35 +0530

    Time: 2 ms (27.44%) | Memory: 57.4 MB (65.96%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,34 +1,43 @@
 class Solution {
-    public void reverse(int[] nums , int low, int high){
-        while(low < high){
-            int temp = nums[low];
-            nums[low] = nums[high];
-            nums[high] = temp;
-            low++;
-            high--;
-        }
-    }
+    // public void reverse(int[] nums , int low, int high){
+    //     while(low < high){
+    //         int temp = nums[low];
+    //         nums[low] = nums[high];
+    //         nums[high] = temp;
+    //         low++;
+    //         high--;
+    //     }
+    // }
+    // public void rotate(int[] nums, int k) {
+    //     int n = nums.length;
+    //     k = k % n;
+    //     reverse(nums, 0, n-1);
+    //     reverse(nums,0,k-1);
+    //     reverse(nums,k,n-1);
+    // }
+
     public void rotate(int[] nums, int k) {
         int n = nums.length;
         k = k % n;
-        reverse(nums, 0, n-1);
-        reverse(nums,0,k-1);
-        reverse(nums,k,n-1);
+        int count = 0; 
+        int start = 0;
+        while (count < n) {
+            int current = start;
+            int prev = nums[start];
+
+            while (true) {
+                int next = (current + k) % n;
+                int temp = nums[next];
+                nums[next] = prev;
+                prev = temp;
+                current = next;
+                count++;
+
+                if (start == current) break; 
+            }
+            start++; 
+        }
     }
 
-
-
-    //TLE 
-    // public void rotate(int[] nums, int k) {
-    //     int n = nums.length;
-    //     k = k % n;  
-    //     while (k > 0) {
-    //         int last = nums[n - 1];
-    //         for (int i = n - 1; i > 0; i--) {
-    //             nums[i] = nums[i - 1];
-    //         }
-    //         nums[0] = last;
-    //         k--;
-    //     }
-    // }
+    
 }
