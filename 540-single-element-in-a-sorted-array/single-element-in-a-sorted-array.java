a9b547467f6ab8719829d63f21950b8ad124cb4a
Charu Gupta <118685509+charugupta94@users.noreply.github.com>
2025-06-18 10:07:28 +0530

    Time: 0 ms (100.00%) | Memory: 50 MB (90.77%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -3,8 +3,8 @@ class Solution {
         int l = 0;
         int r = arr.length - 1;
         while(l < r){
-            int m = l + (r- l)% 2;
-            if(m % 2 != 0)m--;
+            int m = l + (r- l)/2;
+            if(m % 2 == 1 )m--;
 
             if(arr[m] == arr[m+1]){
                 l = m + 2;
