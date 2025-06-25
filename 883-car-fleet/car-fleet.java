bc939bc58c2baf4abe7541ff351e0a235f128deb
Charu Gupta <118685509+charugupta94@users.noreply.github.com>
2025-06-25 13:53:09 +0530

    Time: 48 ms (90.96%) | Memory: 63.6 MB (5.55%) - LeetSync

--- 883-car-fleet/car-fleet.java
+++ 883-car-fleet/car-fleet.java
@@ -1,19 +1,19 @@
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
         HashMap<Integer , Integer> mp = new HashMap<>();
-        for(int  i =0 ;i< position.length ; i++){
+        int  n = position.length;
+        for(int  i =0 ;i< n ; i++){
             mp.put(position[i], speed[i]);
         }
         Arrays.sort(position);
-        double t[] = new double[position.length];
-        for(int i = 0; i< position.length ;i++){
-            t[i] = (double)(target - position[i]) /(double) mp.get(position[i]); 
-            System.out.print(t[i]);
+        double t[] = new double[n];
+        for(int i = 0; i< n ;i++){
+            t[i] = (double)(target - position[i]) /(double) mp.get(position[i]);
         }
 
         int count = 1;
-        double x = t[t.length - 1];
-        for(int  i = t.length - 2; i>=0 ;i--){
+        double x = t[n - 1];
+        for(int  i =n - 2; i>=0 ;i--){
             if(t[i] > x){
                 count++;
                 x = t[i];
