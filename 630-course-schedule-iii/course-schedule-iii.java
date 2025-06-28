20c408506ff9d012ce12a28e139b5b4289c384d2
Charu Gupta <118685509+charugupta94@users.noreply.github.com>
2025-06-28 16:29:59 +0530

    Time: 296 ms (5.15%) | Memory: 52 MB (100.00%) - LeetSync

---
+++ 630-course-schedule-iii/course-schedule-iii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int scheduleCourse(int[][] courses) {
+        int d = 0;
+        int count = 0;
+        Arrays.sort(courses,(a,b)->a[1]-b[1]);
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i = 0; i< courses.length;i++){
+            if(courses[i][0] <= courses[i][1] && d+courses[i][0] <= courses[i][1]){
+                d += courses[i][0];
+                pq.add(courses[i][0]);
+                count++;
+            }
+            else if(!pq.isEmpty()){
+                int max = pq.peek();
+                if(max > courses[i][0]){
+                    d -= pq.poll();
+                    d += courses[i][0];
+                    pq.add(courses[i][0]);
+                }
+            }
+            System.out.println(d);
+        }
+        return count; 
+    }
+}
