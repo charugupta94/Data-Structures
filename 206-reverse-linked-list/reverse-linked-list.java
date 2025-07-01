47a5aaf4841e367370de17acfd98038a4ee2ae0f
Charu Gupta <118685509+charugupta94@users.noreply.github.com>
2025-07-01 10:06:08 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (11.28%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -9,17 +9,26 @@
  * }
  */
 class Solution {
+    // public ListNode reverseList(ListNode head) {
+    //     ListNode curr = head;
+    //     ListNode prev = null;
+    //     ListNode next = null;
+    //     while(curr != null)
+    //     {
+    //         next = curr.next;
+    //         curr.next = prev;
+    //         prev = curr;
+    //         curr = next;
+    //     }
+    //     return prev;
+    // }
+
+
     public ListNode reverseList(ListNode head) {
-        ListNode curr = head;
-        ListNode prev = null;
-        ListNode next = null;
-        while(curr != null)
-        {
-            next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-        return prev;
+        if(head == null || head.next == null)return head;
+        ListNode temp = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return temp;
     }
 }
