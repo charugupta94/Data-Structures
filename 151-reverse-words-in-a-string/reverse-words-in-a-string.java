e6de969196ae4e2ab5356c3df074440320542057
Charu Gupta <118685509+charugupta94@users.noreply.github.com>
2025-07-06 00:18:13 +0530

    Time: 7 ms (56.23%) | Memory: 43 MB (79.28%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,32 +1,44 @@
 class Solution {
-    public String reverseWords(String s) {
-        s = reverse(s);
-        StringBuilder ans = new StringBuilder();
-        String words[] = s.split("\\s+");
-        for(int i =0 ;i< words.length ;i++){
-            String c = words[i];
-            c = reverse(c);
-            if(i < words.length - 1){
-                ans.append(c).append(" ");
-            }
-            else{
-                ans.append(c);
-            }
-        }
-        return ans.toString().trim();
-    }
-    public String reverse(String s){
-        StringBuilder sb = new StringBuilder(s);
-        int l = 0;
-        int r = sb.length() - 1;
+    // public String reverseWords(String s) {
+    //     s = reverse(s);
+    //     StringBuilder ans = new StringBuilder();
+    //     String words[] = s.split("\\s+");
+    //     for(int i =0 ;i< words.length ;i++){
+    //         String c = words[i];
+    //         c = reverse(c);
+    //         if(i < words.length - 1){
+    //             ans.append(c).append(" ");
+    //         }
+    //         else{
+    //             ans.append(c);
+    //         }
+    //     }
+    //     return ans.toString().trim();
+    // }
+    // public String reverse(String s){
+    //     StringBuilder sb = new StringBuilder(s);
+    //     int l = 0;
+    //     int r = sb.length() - 1;
         
-        while (l < r) {
-            char c = sb.charAt(l);
-            sb.setCharAt(l, sb.charAt(r));
-            sb.setCharAt(r, c);
-            l++;
-            r--;
+    //     while (l < r) {
+    //         char c = sb.charAt(l);
+    //         sb.setCharAt(l, sb.charAt(r));
+    //         sb.setCharAt(r, c);
+    //         l++;
+    //         r--;
+    //     }
+    //     return sb.toString();
+    // }
+
+    public String reverseWords(String s) {
+        StringBuilder reversed = new StringBuilder();
+        String[] words = s.trim().split("\\s+");
+        for(int i = words.length - 1; i >= 0;i--){
+            reversed.append(words[i]);
+            if( i > 0){
+                reversed.append(" ");
+            }
         }
-        return sb.toString();
+        return reversed.toString();
     }
 }
