7d4b3fc2304442a42ac5855a2a2d9e0354b0aaa8
Charu Gupta <118685509+charugupta94@users.noreply.github.com>
2025-07-17 19:56:24 +0530

    Time: 1 ms (100.00%) | Memory: 46.5 MB (73.77%) - LeetSync

--- 2737-row-with-maximum-ones/row-with-maximum-ones.java
+++ 2737-row-with-maximum-ones/row-with-maximum-ones.java
@@ -1,31 +1,56 @@
 class Solution {
     public int[] rowAndMaximumOnes(int[][] mat) {
-        HashMap<Integer, Integer> mp = new HashMap<>();
-        for(int i =0 ;i<mat.length ;i++){
+        int [] ans = new int [2];
+        int index = 0;
+        int maxCount = -1;
+
+        for (int i = 0; i < mat.length; i++) {
             int count = 0;
-            for(int j = 0;j<mat[0].length ;j++){
-                if(mat[i][j] == 1){
-                    count++;
-                }
+            for (int n : mat[i]) {
+                count += n;
             }
-            mp.put(i,count);
-        }
-        int row = -1;
-        int max= -1;
 
-        for(Map.Entry<Integer, Integer> entry : mp.entrySet()){
-            int rowidx = entry.getKey();
-            int count = entry.getValue();
-            
-            if(count > max || (count == max && rowidx < row)){
-                max = count;
-                row = rowidx;
+            if (count > maxCount) {
+                index = i;
+                maxCount = count;
             }
+
         }
 
-        int[] res = new int[2];
-        res[0] = row;
-        res[1] = max;
-        return res;
+        ans[0] = index;
+        ans[1] = maxCount;
+
+        return ans;
+        
     }
+
+    // public int[] rowAndMaximumOnes(int[][] mat) {
+    //     HashMap<Integer, Integer> mp = new HashMap<>();
+    //     for(int i =0 ;i<mat.length ;i++){
+    //         int count = 0;
+    //         for(int j = 0;j<mat[0].length ;j++){
+    //             if(mat[i][j] == 1){
+    //                 count++;
+    //             }
+    //         }
+    //         mp.put(i,count);
+    //     }
+    //     int row = -1;
+    //     int max= -1;
+
+    //     for(Map.Entry<Integer, Integer> entry : mp.entrySet()){
+    //         int rowidx = entry.getKey();
+    //         int count = entry.getValue();
+            
+    //         if(count > max || (count == max && rowidx < row)){
+    //             max = count;
+    //             row = rowidx;
+    //         }
+    //     }
+
+    //     int[] res = new int[2];
+    //     res[0] = row;
+    //     res[1] = max;
+    //     return res;
+    // }
 }
