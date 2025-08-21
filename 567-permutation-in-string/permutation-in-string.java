e6fd5e4cb00fe4d68b25135a18d240e6245288de
Charu Gupta <118685509+charugupta94@users.noreply.github.com>
2025-08-22 01:18:36 +0530

    Time: 5 ms (96.02%) | Memory: 42.7 MB (67.98%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -5,17 +5,37 @@ class Solution {
 
         if(n > m)return false;
 
-        char[] ch = s1.toCharArray();
-        Arrays.sort(ch);
-        String s = new String(ch);
+        // char[] ch = s1.toCharArray();
+        // Arrays.sort(ch);
+        // String s = new String(ch);
 
-        for(int i =0; i<= m-n ;i++){
-            String subs = s2.substring(i , i+n);
-            char[] wor = subs.toCharArray();
-            Arrays.sort(wor);
-            String new_s = new String(wor);
+        // for(int i =0; i<= m-n ;i++){
+        //     String subs = s2.substring(i , i+n);
+        //     char[] wor = subs.toCharArray();
+        //     Arrays.sort(wor);
+        //     String new_s = new String(wor);
 
-            if(s.equals(new_s))return true; 
+        //     if(s.equals(new_s))return true; 
+        // }
+        // return false;
+
+        int[] freq_s1 = new int[26];
+        int[] freq_s2 = new int[26];
+        for(char ch : s1.toCharArray()){
+            freq_s1[ch - 'a']++;
+        }
+        int left = 0;
+        int right =0;
+        while(right < m){
+            if(right - left + 1 > n){
+                freq_s2[s2.charAt(left) - 'a']--;
+                left++;
+            }
+            freq_s2[s2.charAt(right) - 'a']++;
+            if(Arrays.equals(freq_s1 , freq_s2)){
+                return true;
+            }
+            right++;
         }
         return false;
     }
